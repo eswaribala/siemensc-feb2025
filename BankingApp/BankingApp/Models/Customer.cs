09c305c496004c13dcc6ccdfaa094a45115e4b9f
Parameswari Bala <parameswaribala@gmail.com>
2025-02-25 15:33:25 +0530

    updated

--- BankingApp/BankingApp/Models/Customer.cs
+++ BankingApp/BankingApp/Models/Customer.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -13,22 +15,25 @@ namespace BankingApp.Models
      * Abstract class object cannot be created
      * But We can inherit
      */
+    [Table("Customer")]
     internal abstract class Customer
     {
         //sttaic variable
         //Field
-        public static string BankName;
+        //public static string BankName;
 
-        internal bool IsActive {  get; set; }
+       // internal bool IsActive {  get; set; }
 
-        public Customer(long accountNo, FullName fullName)
-        {
-            AccountNo = accountNo;
-            FullName = fullName;
-        }
-
-        protected long AccountNo { get; }
-        protected FullName FullName { get; }
+        //public Customer(long accountNo, FullName fullName)
+        //{
+        //    AccountNo = accountNo;
+        //    FullName = fullName;
+        //}
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Account_No")]
+        protected long AccountNo { get; set; }
+        protected FullName FullName { get; set; }
         protected string Email {  get; set; }
         protected long PhoneNumber { get; set; }
 
@@ -36,17 +41,17 @@ namespace BankingApp.Models
 
         protected string Password {  get; set; }
 
-        protected abstract float ComputeInterest();
-        //Basic implementation
-        protected virtual float OTP() {
+        //protected abstract float ComputeInterest();
+        ////Basic implementation
+        //protected virtual float OTP() {
 
-            return Faker.RandomNumber.Next(1000,9999);
+        //    return Faker.RandomNumber.Next(1000,9999);
         
-        }
+        //}
 
-        public override string ToString()
-        {
-            return JsonConvert.SerializeObject(this,Formatting.Indented);
-        }
+        //public override string ToString()
+        //{
+        //    return JsonConvert.SerializeObject(this,Formatting.Indented);
+        //}
     }
 }
--- BankingApp/BankingApp/Models/FullName.cs
+++ BankingApp/BankingApp/Models/FullName.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BankingApp.Models
 {
+    [Owned]
     internal class FullName
     {
+        [Column("First_Name",TypeName ="varchar(100)")]
+        [Required]
+        [RegularExpression("^[a-zA-Z]{3,100}$", ErrorMessage = "First name should be between 3 to 100 chars")]
         public string FirstName { get; set; }
+        [Column("Last_Name", TypeName = "varchar(100)")]
+        [RegularExpression("^[a-zA-Z]{3,100}$", ErrorMessage = "Last name should be between 3 to 100 chars")]
+        [Required]
         public string LastName { get; set; }
+        [Column("Middle_Name", TypeName = "varchar(100)")]
+
         public string MiddleName {  get; set; }
     }
 }
