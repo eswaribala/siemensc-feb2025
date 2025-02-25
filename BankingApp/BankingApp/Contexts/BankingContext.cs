acac4a1197d00d71624265a8f65ef78acb4c0d91
Parameswari Bala <parameswaribala@gmail.com>
2025-02-25 16:17:34 +0530

    updated

---
+++ BankingApp/BankingApp/Contexts/BankingContext.cs
@@ -0,0 +1,32 @@
+﻿using BankingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Contexts
+{
+    internal class BankingContext:DbContext
+    {
+        public BankingContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { 
+        
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Individual> Individuals { get; set; }
+        public DbSet<Corporate> Corporates { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Individual>().ToTable("Individual");
+            modelBuilder.Entity<Corporate>().ToTable("Corporate");
+            modelBuilder.Entity<Address>().ToTable("Address");
+        }
+    }
+}
--- BankingApp/BankingApp/Program.cs
+++ BankingApp/BankingApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BankingApp.Contexts;
 using BankingApp.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http.Headers;
 
 var configuration=new ConfigurationBuilder()
@@ -8,49 +11,15 @@ var configuration=new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
 
+var services=new ServiceCollection();
+services.AddDbContext<BankingContext>(options =>
+options.UseSqlServer(configuration.GetConnectionString("DefaultConn")));
+var provider=services.BuildServiceProvider();
 
 
 
 
 
 
-
-//create 100 individuals sort them based dob
-Individual[] individuals=new Individual[100];
-
-Gender gender;
-for(int i = 0; i < 100; i++)
-{
-    if (i % 2 == 0)
-      gender = Gender.MALE;
-    else
-        gender = Gender.FEMALE;
-
-    individuals[i] = new Individual(Faker.RandomNumber.Next(1000, 100000),
-        new FullName
-        {
-            FirstName = Faker.Name.First(),
-            LastName = Faker.Name.Last(),
-            MiddleName = Faker.Name.Middle(),
-        });
-    individuals[i].DOB = Faker.Identification.DateOfBirth();
-    individuals[i].Gender = gender;
-    individuals[i].SetValues(Faker.Internet.Email(), Faker.Identification.UsPassportNumber(),
-        Faker.RandomNumber.Next(90000000000,999999999),new Address
-        {
-            DoorNo=Faker.Address.SecondaryAddress(),
-            StreetName=Faker.Address.StreetName(),
-            City=Faker.Address.City(),
-            Region=Faker.Address.CaProvince(),
-            Country=Faker.Address.Country(),
-            PostalCode=Faker.Address.ZipCode(),
-        });
-
-
-   
-}
-Array.Sort(individuals);
-
-individuals.ToList().ForEach(x => Console.WriteLine(x));
 
 Console.ReadKey();
